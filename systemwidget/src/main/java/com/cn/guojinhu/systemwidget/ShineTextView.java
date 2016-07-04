9c66978de975e0213ea2dcebfcace0152a28bde9
Vo7ice <Vo7ice@outlook.com>
2016-07-04 20:27:56 +0800

    add shineTextView

---
+++ systemwidget/src/main/java/com/cn/guojinhu/systemwidget/ShineTextView.java
@@ -0,0 +1,70 @@
+package com.cn.guojinhu.systemwidget;
+
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Shader;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+public class ShineTextView  extends TextView{
+
+    private LinearGradient mLinearGradient;
+    private Matrix mGradientMatrix;
+    private Paint mPaint;
+    private int mViewWidth = 0;
+    private int mTranslate = 0;
+
+    public ShineTextView(Context context) {
+        super(context);
+    }
+
+    public ShineTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ShineTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        if (mViewWidth == 0) {//当宽度为0的时候重新测量
+            mViewWidth = getMeasuredWidth();
+            if (mViewWidth > 0) {
+                mPaint = getPaint();//获得画笔
+                mLinearGradient = new LinearGradient(
+                        0,
+                        0,
+                        mViewWidth,
+                        0,
+                        new int[]{
+                                Color.BLUE, 0xffffffff,
+                                Color.BLUE},
+                        null,
+                        Shader.TileMode.CLAMP);//初始化线性渲染
+                mPaint.setShader(mLinearGradient);
+                mGradientMatrix = new Matrix();//初始化线性渲染
+            }
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if (mGradientMatrix != null) {
+            mTranslate += mViewWidth / 5;//每次+5分之1的宽度
+            if (mTranslate > 2 * mViewWidth) {//当平移超出两倍的宽度时,往回走
+                mTranslate = -mViewWidth;
+            }
+            mGradientMatrix.setTranslate(mTranslate, 0);
+            mLinearGradient.setLocalMatrix(mGradientMatrix);
+            postInvalidateDelayed(100);//延迟100ms刷新UI
+        }
+    }
+}
