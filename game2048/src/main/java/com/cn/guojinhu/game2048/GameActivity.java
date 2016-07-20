f8397ed89f395218d3a0e9a8d3369462a0e54e21
Vo7ice <Vo7ice@outlook.com>
2016-07-20 20:43:32 +0800

    init 2048

---
+++ game2048/src/main/java/com/cn/guojinhu/game2048/GameActivity.java
@@ -0,0 +1,15 @@
+package com.cn.guojinhu.game2048;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+
+    }
+}
