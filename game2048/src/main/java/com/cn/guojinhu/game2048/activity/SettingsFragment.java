6c232bb1d1232bf3bb714d0a78371b890a5589ff
Vo7ice <Vo7ice@outlook.com>
2016-07-27 19:53:42 +0800

    modify settings and headup notification

--- game2048/src/main/java/com/cn/guojinhu/game2048/activity/SettingsFragment.java
+++ game2048/src/main/java/com/cn/guojinhu/game2048/activity/SettingsFragment.java
@@ -1,18 +1,12 @@
 package com.cn.guojinhu.game2048.activity;
 
-import android.preference.PreferenceFragment;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
 
 import com.cn.guojinhu.game2048.R;
 
-/**
- * A placeholder fragment containing a simple view.
- */
-public class SettingsFragment extends PreferenceFragment {
+public class SettingsFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
 
     public SettingsFragment() {
     }
@@ -21,5 +15,10 @@ public class SettingsFragment extends PreferenceFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.ic_settings);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
     }
 }
--- notificationdemo/src/main/java/com/cn/guojinhu/notificationdemo/MainActivity.java
+++ notificationdemo/src/main/java/com/cn/guojinhu/notificationdemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.cn.guojinhu.notificationdemo;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
@@ -45,9 +46,12 @@ public class MainActivity extends AppCompatActivity {
                 .setTicker("You clicked BaseNF!")
                 .setContentTitle("这是通知标题")
                 .setContentText("这是通知内容")
+                /*.setOngoing(true)*/
+                .setFullScreenIntent(createIntent(MainActivity.this), true)
                 .setWhen(System.currentTimeMillis())
                 .setColor(Color.RED)
                 .build();
+        notification.extras.putInt("headsup", 0);//取消head-up风格通知,使fullScreenIntent起效
         mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -74,6 +78,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private PendingIntent createIntent(Context context) {
+        Intent intent = new Intent(context, TargetActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        return PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
     }
 
     @Override
