a3db56e9b2092617ba68af4a509e887b0cee92ba
Vo7ice <Vo7ice@outlook.com>
2016-09-21 17:09:30 +0800

    WinXLoading finish

--- systemwidget/src/main/java/com/cn/guojinhu/systemwidget/view/WinXLoadingView.java
+++ systemwidget/src/main/java/com/cn/guojinhu/systemwidget/view/WinXLoadingView.java
@@ -9,6 +9,7 @@ import android.graphics.Path;
 import android.graphics.PathMeasure;
 import android.graphics.RectF;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 /**
@@ -20,6 +21,7 @@ public class WinXLoadingView extends View {
     private Paint mPaint;
 
     private Path mPath;
+    private Path dst;
 
     private PathMeasure mPathMeasure;
 
@@ -41,6 +43,7 @@ public class WinXLoadingView extends View {
     public WinXLoadingView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         init();
+        valueAnimator.start();
     }
 
     public WinXLoadingView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
@@ -58,6 +61,7 @@ public class WinXLoadingView extends View {
         RectF oval = new RectF(-150, -150, 150, 150);
         mPath.addArc(oval, -90, 359.9f);
         mPathMeasure = new PathMeasure(mPath, false);
+        dst = new Path();
 
         initAnimate();
     }
@@ -72,16 +76,64 @@ public class WinXLoadingView extends View {
                 invalidate();
             }
         });
-        valueAnimator.start();
+
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.translate(getWidth() / 2, getHeight() / 2);
-        Path dst = new Path();
-        mPathMeasure.getSegment(mPathMeasure.getLength() * progress,
-                mPathMeasure.getLength() * progress + 1, dst, true);
+        canvas.translate(mWidth / 2, mHeight / 2);
+
+        if (progress > 0.95) {
+            canvas.drawPoint(0, -150, mPaint);//在快结束的时候先绘制一个点
+        }
+
+        dst.reset();
+        drawEachPath(canvas);
+
+        /*mPathMeasure.getSegment(mPathMeasure.getLength() * progress,
+                mPathMeasure.getLength() * progress + 1, dst, true);*/
         canvas.drawPath(dst, mPaint);
     }
+
+    private void drawEachPath(Canvas canvas) {
+        int num = (int) (progress / (0.05f));
+        float x, y, s;
+        switch (num) {
+            default:
+            case 3:
+                x = progress - (0.15f) * (1 - progress);//偏移方程 让最后点集合在一起 y=k*x+b
+                s = mPathMeasure.getLength();
+                y = -s * x * x + 2 * x * s;//抛物线方程 速度不断增加 y=a*x*x+b*x+c
+                mPathMeasure.getSegment(y, y + 1, dst, true);
+                //break;//不加会只有一部分
+            case 2:
+                x = progress - (0.10f) * (1 - progress);
+                s = mPathMeasure.getLength();
+                y = -s * x * x + 2 * x * s;
+                mPathMeasure.getSegment(y, y + 1, dst, true);
+                //break;
+            case 1:
+                x = progress - (0.05f) * (1 - progress);
+                s = mPathMeasure.getLength();
+                y = -s * x * x + 2 * x * s;
+                mPathMeasure.getSegment(y, y + 1, dst, true);
+                //break;
+            case 0:
+                x = progress;
+                s = mPathMeasure.getLength();
+                y = -s * x * x + 2 * x * s;
+                mPathMeasure.getSegment(y, y + 1, dst, true);
+                break;
+        }
+        canvas.drawPath(dst, mPaint);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mWidth = w;
+        mHeight = h;
+    }
+
 }
